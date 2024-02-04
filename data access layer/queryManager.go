fd3afe6055cb06f3a95d2513d725879eaa3b90e0
Mahmoud Fredj <newraisingray@gmail.com>
2024-02-04 11:34:01 +0300

    add init() func in queryManager

--- data access layer/queryManager.go
+++ data access layer/queryManager.go
@@ -1,1 +1,17 @@
 package dal
+
+import (
+	"database/sql"
+	"log"
+)
+
+var db *sql.DB
+
+func Init(db *sql.DB, url string) {
+	var err error
+	db, err = sql.Open("sqlite3", url)
+	if err != nil {
+		log.Fatal("Error in initiating db connection QueryManager")
+	}
+
+}
